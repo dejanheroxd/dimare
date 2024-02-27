ba3022d2b9559cd78133919650cd153b12bb2ecb
dejan <yugihohgx2@gmail.com>
2024-02-27 13:30:14 +0200

    responsive about section

--- src/pages/home/about-sec/AboutSec.jsx
+++ src/pages/home/about-sec/AboutSec.jsx
@@ -4,24 +4,29 @@ import crab from "../../../assets/crab.png";
 
 export default function AboutSec() {
   return (
-    <div className="bg-[#EEF9FD] pb-4 overflow-hidden">
-      <div className="p-4 pt-20">
-        <img src={aboutFish} alt="" />
+    <div className="bg-[#EEF9FD] 2xl:bg-white relative xl:flex xl:justify-center xl:gap-x-10 pb-4 overflow-hidden">
+      <div className="2xl:bg-[#e1f7ff] absolute w-[1000px] left-[-320px] top-[-230px] z-0 h-[900px]"></div>
+      <div className="p-4 pt-20 z-10">
+        <img
+          className="max-w-[500px] xl:ml-10 xl:w-[800px] z-10 w-full"
+          src={aboutFish}
+          alt=""
+        />
       </div>
-      <div className="p-4 pt-[64px] relative">
-        <div className="flex relative">
+      <div className="p-4 pt-[64px] relative xl:static xl:flex xl:flex-col xl:justify-center xl:items-start">
+        <div className="flex relative md:text-xl">
           <p>ABOUT RESTAURANT</p>
           <div className=" absolute left-[165px] top-[18px]"></div>
         </div>
-        <p className="text-3xl pt-4 pb-8">
-          Enjoy An Exceptional Journey of Taste
+        <p className="text-3xl pt-4 md:pt-2 pb-8 xl:w-[700px] z-10 md:text-5xl md:leading-[60px] md:-ml-[5px]">
+          ENJOY AN EXCEPTIONAL JOURNEY OF TASTE
         </p>
         <img
-          className="absolute w-[240px] h-[110px] right-[-90px] rotate-[-50deg] top-4"
+          className="absolute w-[240px] h-[110px] md:w-[380px] md:h-[170px] md:right-[-130px] md:top-[-50px] right-[-90px] rotate-[-50deg] xl:w-[480px] xl:h-[200px] xl:top-[-13px] xl:right-[-145px] 2xl:w-[650px] 2xl:h-[300px] top-4"
           src={crab}
           alt=""
         />
-        <div className="text-gray-700">
+        <div className="text-gray-700 xl:w-[560px] z-10">
           Our buzzy food-hall style concept is inspired by international dining
           styles, especially in Asia. Explore the following fast-action food
           stations as busy chefs perform.
