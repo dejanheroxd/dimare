bbdf2fd771c578cb71f7fea6fa6e6cf0ff962681
dejan <yugihohgx2@gmail.com>
2024-02-27 14:52:26 +0200

    added animations

--- src/pages/home/about-sec/AboutSec.jsx
+++ src/pages/home/about-sec/AboutSec.jsx
@@ -1,32 +1,55 @@
 import React from "react";
 import aboutFish from "../../../assets/aboutFish.jpg";
 import crab from "../../../assets/crab.png";
+import { motion } from "framer-motion";
 
 export default function AboutSec() {
   return (
     <div className="bg-[#EEF9FD] 2xl:bg-white relative xl:flex xl:justify-center xl:gap-x-10 pb-4 overflow-hidden">
       <div className="2xl:bg-[#e1f7ff] absolute w-[1000px] left-[-320px] top-[-230px] z-0 h-[900px]"></div>
       <div className="p-4 pt-20 z-10">
-        <img
-          className="max-w-[500px] xl:ml-10 xl:w-[800px] z-10 w-full"
-          src={aboutFish}
-          alt=""
-        />
+        <motion.div
+          initial={{ opacity: 0, x: -70 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <img
+            className="max-w-[500px] xl:ml-10 xl:w-[800px] z-10 w-full"
+            src={aboutFish}
+            alt=""
+          />
+        </motion.div>
       </div>
       <div className="p-4 pt-[64px] relative xl:static xl:flex xl:flex-col xl:justify-center xl:items-start">
         <div className="flex relative md:text-xl">
-          <p>ABOUT RESTAURANT</p>
+          <motion.p
+            initial={{ opacity: 0, y: 70 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.7 }}
+          >
+            ABOUT RESTAURANT
+          </motion.p>
           <div className=" absolute left-[165px] top-[18px]"></div>
         </div>
-        <p className="text-3xl pt-4 md:pt-2 pb-8 xl:w-[700px] z-10 md:text-5xl md:leading-[60px] md:-ml-[5px]">
+        <motion.p
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+          className="text-3xl pt-4 md:pt-2 pb-8 xl:w-[700px] z-10 md:text-5xl md:leading-[60px] md:-ml-[5px]"
+        >
           ENJOY AN EXCEPTIONAL JOURNEY OF TASTE
-        </p>
+        </motion.p>
         <img
           className="absolute w-[240px] h-[110px] md:w-[380px] md:h-[170px] md:right-[-130px] md:top-[-50px] right-[-90px] rotate-[-50deg] xl:w-[480px] xl:h-[200px] xl:top-[-13px] xl:right-[-145px] 2xl:w-[650px] 2xl:h-[300px] top-4"
           src={crab}
           alt=""
         />
-        <div className="text-gray-700 xl:w-[560px] z-10">
+        <motion.div
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+          className="text-gray-700 xl:w-[560px] z-10"
+        >
           Our buzzy food-hall style concept is inspired by international dining
           styles, especially in Asia. Explore the following fast-action food
           stations as busy chefs perform.
@@ -38,7 +61,7 @@ export default function AboutSec() {
             spirits, beers and wines are all made with integrity and offer
             something for every guest.
           </p>
-        </div>
+        </motion.div>
         <button className="bg-fishBlue text-white h-[50px] mt-9 px-6">
           READ MORE ABOUT US
         </button>
--- src/pages/home/openingTime-sec/OpeningTime.jsx
+++ src/pages/home/openingTime-sec/OpeningTime.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import kalamari from "../../../assets/kalamari.jpg";
 import seafood from "../../../assets/seafood.jpg";
+import { motion } from "framer-motion";
 
 export default function OpeningTime() {
   return (
@@ -9,34 +9,64 @@ export default function OpeningTime() {
         <img src="" alt="" />
       </div>
       <div>
-        <p className="text-3xl pt-10 md:text-5xl md:-ml-[5px]">
-          OPEN TIME HOURS
-        </p>
-        <p className="pt-5">
-          Come and experience the classy atmosphere with delicious food
-        </p>
-        <p className="py-3">OPENING HOUR:</p>
-        <p className="pb-1">
-          Monday - Friday
-          <span className="text-gray-500"> : 9.00pm - 22:00pm</span>
-        </p>
-        <p className="pb-1">
-          Saturday<span className="text-gray-500"> : 10.00pm - 23:00pm</span>
-        </p>
-        <p className="pb-1">
-          Sunday<span className="text-gray-500"> : 5.00pm - 23:00pm</span>
-        </p>
-        <p className="pb-1">
-          Holidays<span className="text-gray-500"> : Closed</span>
-        </p>
+        <motion.div
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <p className="text-3xl pt-10 md:text-5xl md:-ml-[5px]">
+            OPEN TIME HOURS
+          </p>
+          <p className="pt-5">
+            Come and experience the classy atmosphere with delicious food
+          </p>
+        </motion.div>
+        <motion.p
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+          className="py-3"
+        >
+          OPENING HOUR:
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <p className="pb-1">
+            Monday - Friday
+            <span className="text-gray-500"> : 9.00pm - 22:00pm</span>
+          </p>
+          <p className="pb-1">
+            Saturday<span className="text-gray-500"> : 10.00pm - 23:00pm</span>
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          <p className="pb-1">
+            Sunday<span className="text-gray-500"> : 5.00pm - 23:00pm</span>
+          </p>
+          <p className="pb-1">
+            Holidays<span className="text-gray-500"> : Closed</span>
+          </p>
+        </motion.div>
         <button className="bg-fishBlue text-white h-[50px] mt-9 px-6">
           BOOK A TABLE
         </button>
       </div>
       <div className="2xl:bg-[#e1f7ff] absolute w-[600px] right-[-50px] top-[200px] z-0 h-[650px]"></div>
-      <div className="xl:mr-[-170px] relative z-10">
+      <motion.div
+        initial={{ opacity: 0, x: 70 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.7 }}
+        className="xl:mr-[-170px] relative z-10"
+      >
         <img className="pt-8 pb-10 xl:w-[950px]" src={seafood} alt="" />
-      </div>
+      </motion.div>
     </div>
   );
 }
